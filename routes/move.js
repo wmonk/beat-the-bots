e761fd5dc754741ab87be8008102a362c1157506
Julian Haeger <julianhaeger@gmail.com>
2014-11-12 09:47:15 +0000

    bets every time

--- routes/move.js
+++ routes/move.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/move', function(req, res) {
-  res.send('moving');
+router.get('/move', function (req, res) {
+	var game = require('../lib/game').current();
+	var move = game.move();
+	res.send(move);
 });
 
 module.exports = router;
