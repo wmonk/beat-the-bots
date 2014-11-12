997ff4c6206fe7fc745a0f197b78e7dec655263e
Will Monk <will@williammonk.co.uk>
2014-11-12 11:06:59 +0000

    added updates

--- routes/update.js
+++ routes/update.js
@@ -4,9 +4,17 @@ var router = express.Router();
 /* GET users listing. */
 router.post('/update', function (req, res) {
 	var currentGame = require('../lib/game').current();
-
 	if (!currentGame) {
 		return res.status(404).send('no game started');
+	}
+
+	var command = req.param('COMMAND');
+	var data = req.param('DATA');
+
+	if (command === 'CARD') {
+		currentGame.update({
+			card: data
+		});
 	}
 
 	res.send('updating');
