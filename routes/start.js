c44e4ee76f8b5003de893206b552cbeb35ab8192
Julian Haeger <julianhaeger@gmail.com>
2014-11-12 09:37:28 +0000

    created a game module to represent a running game

--- routes/start.js
+++ routes/start.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var game = require('../lib/game');
 
 /* GET users listing. */
-router.post('/start', function(req, res) {
-  res.send('starting');
+router.post('/start', function (req, res) {
+	game.startNew();
+	res.send('starting');
 });
 
 module.exports = router;
--- routes/update.js
+++ routes/update.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.post('/update', function(req, res) {
-  res.send('updating');
+router.post('/update', function (req, res) {
+	var currentGame = require('../lib/game').current();
+	res.send('updating');
 });
 
 module.exports = router;
